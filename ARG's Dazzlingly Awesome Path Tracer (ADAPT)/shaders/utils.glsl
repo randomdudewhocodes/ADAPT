d93f761c2c07cb540db941d57eafde71294d0a33
randomdudewhocodes <nguyen07112007@gmail.com>
2025-02-04 15:42:37 +0700

    Fix NaNs

--- ARG's Dazzlingly Awesome Path Tracer (ADAPT)/shaders/utils.glsl
+++ ARG's Dazzlingly Awesome Path Tracer (ADAPT)/shaders/utils.glsl
@@ -5,3 +5,13 @@ const float TAU    = 6.28318530717958647693,
 
 #define sqr(x) (x) * (x)
 #define dot2(x) dot(x, x)
+
+float deNaN (float v)
+{
+    return v != v ? 0. : v;
+}
+
+vec3 deNaN(vec3 v)
+{
+    return vec3(deNaN(v.x), deNaN(v.y), deNaN(v.z));
+}
